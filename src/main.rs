a9fc5044c8d5fb00b035a8d0fea14a78edd33182
Evan Cameron <cameron.evan@gmail.com>
2016-03-27 16:00:31 -0400

    add mnemonic code

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
 extern crate iron;
 extern crate router;
 
-use iron::{Iron, Request, Response, IronResult};
+use iron::prelude::*;
 use iron::status;
-use router::{Router};
+use router::Router;
 
 fn main() {
     let mut router = Router::new();
     router.get("/", handler);
-    router.get("/:query", query_handler);
+    router.get("/:query", handler);
 
     Iron::new(router).http("localhost:3000").unwrap();
 
-    fn handler(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "OK")))
-    }
-
-    fn query_handler(req: &mut Request) -> IronResult<Response> {
-        let ref query = req.extensions.get::<Router>()
-            .unwrap().find("query").unwrap_or("/");
-        Ok(Response::with((status::Ok, *query)))
+    fn handler(req: &mut Request) -> IronResult<Response> {
+        let query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
+        Ok(Response::with((status::Ok, query)))
     }
 }
