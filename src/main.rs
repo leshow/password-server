207ec0dfe586305d7ccf53033ed21f578d7f483c
Evan Cameron <cameron.evan@gmail.com>
2016-04-14 22:01:08 -0400

    add static file serving

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate iron;
 extern crate router;
 extern crate logger;
+extern crate staticfile;
+extern crate mount;
 
 extern crate lib;
 
@@ -8,26 +10,32 @@ use iron::prelude::*;
 use iron::status;
 use router::Router;
 use logger::Logger;
+use staticfile::Static;
+use mount::Mount;
 
 use lib::{MnemonicBuilder, Mnemonic};
+
+use std::path::Path;
 
 fn main() {
     let (logger_before, logger_after) = Logger::new(None);
     let builder: MnemonicBuilder = MnemonicBuilder::new().expect("Failed to open wordlist.");
 
+    // set up routes
     let mut router = Router::new();
-    router.get("/", handler);
     router.get("/generate", move |r: &mut Request| generate_handler(r, &builder));
 
+    // add logger middleware
     let mut chain = Chain::new(router);
     chain.link_before(logger_before);
     chain.link_after(logger_after);
+    // set up mount for static file serving
+    let mut mounts = Mount::new();
+    mounts
+        .mount("/", chain)
+        .mount("/public", Static::new(Path::new("frontend/index.html")));
 
-    Iron::new(chain).http("localhost:3000").unwrap();
-}
-
-fn handler(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "OK")))
+    Iron::new(mounts).http("localhost:3000").unwrap();
 }
 
 fn generate_handler(_: &mut Request, builder: &MnemonicBuilder) -> IronResult<Response> {
