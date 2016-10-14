7479dffe828e3d7d847a1166a9c43198f71e543b
Evan Cameron <evan@vectorface.ca>
2016-10-14 14:49:45 -0400

    rename field

--- src/mnemonic.rs
+++ src/mnemonic.rs
@@ -11,7 +11,7 @@ static PBKDF2_ROUNDS: u32 = 2048;
 static PBKDF2_KEY_LEN: usize = 64;
 
 pub struct Mnemonic {
-    pub in_binary: Vec<u8>,
+    pub mnemonic: Vec<u8>,
 }
 
 impl Mnemonic {
@@ -19,7 +19,7 @@ impl Mnemonic {
         let h = Mnemonic::gen_sha256(&chars).from_hex().unwrap();
         let length = chars.len() / 32;
 
-        Mnemonic { in_binary: [chars.as_ref(), &h[..length]].concat() }
+        Mnemonic { mnemonic: [chars.as_ref(), &h[..length]].concat() }
     }
 
     pub fn to_seed(&self, mnemonic: &str, seed_value: &str) -> Vec<u8> {
@@ -34,10 +34,13 @@ impl Mnemonic {
     }
 
     pub fn to_words(&self, wordslist: &[String]) -> Vec<String> {
+        // Some explanation is necessary.. This uses nom's macros to create a function that
+        // creates a parser specifically for grabbing bits 11 at a time, and putting them in a
+        // u16.
         named!(bit_vec<Vec<u16> >, bits!(many0!(take_bits!(u16, 11))));
 
         let mut mnem_words = Vec::new();
-        if let IResult::Done(_, bit_sequence) = bit_vec(self.in_binary.as_slice()) {
+        if let IResult::Done(_, bit_sequence) = bit_vec(self.mnemonic.as_slice()) {
             for idx in bit_sequence.iter() {
                 mnem_words.push(wordslist[*idx as usize].clone());
             }
